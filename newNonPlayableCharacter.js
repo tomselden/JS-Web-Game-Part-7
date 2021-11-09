018d159aea851cf6911b0169a5523c183d690b49
Thomas Selden <thomasselden42@gmail.com>
2021-11-08 20:27:49 -0500

    made walk function in newNonPlayableCharacter.js asynchronous

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,29 +23,43 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkNorth() {
+    async function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkWest() {
+    async function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        await sleep(time)
+        stop()
     }
 
-    function walkSouth() {
+    async function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        await sleep(time)
+        stop()
     }
 
     function stop() {
         direction = null
         element.src = `./assets/red-character/static.gif`
+    }
+
+    function sleep(time){
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })  
     }
 
     return {
